a7fa15dc7d90d671bef54d9e3f45f01e2bdd8f9d
Clifford <tko@tourkick.com>
2014-10-17 07:36:40 -0500

    Fix it
    Plugin Name won't work, needs to be called Section
    
    section.php doesn't need a version (it's in the plugin-name.php file)

--- sections/pl-starter-section/section.php
+++ sections/pl-starter-section/section.php
@@ -1,12 +1,11 @@
 <?php
 /*
-    Plugin Name: PageLines Starter Section
+    Section: PageLines Starter Section
     Author: PageLines
     Author URI: http://pagelines.com
     Description: A starter section for creating new sections.
     Class Name: pl_starter_section
     Demo:
-    Version: 1.0
     Filter: component
 */
 
